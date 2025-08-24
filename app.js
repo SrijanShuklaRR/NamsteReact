ac8d7e13d5eccbffb6f3b17009b849f57ce261db
Srijan Shukla <srijanshukla353@gmail.com>
2025-08-24 21:29:20 +0530

    React functional component

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
-const heading = <h1>React element</h1>
+const Heading = () => {
+    return <h1>React functional component</h1>
+}
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+root.render(<Heading/>)
